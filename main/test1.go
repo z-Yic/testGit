77e270d3702e6f06c5d724302b1d0720a9c2934a
zc <spring_zc@163.com>
2023-02-08 12:04:33 +0800

    test1.go中Println拼写修改

--- main/test1.go
+++ main/test1.go
@@ -3,20 +3,20 @@ package main
 import "fmt"
 
 func test1() {
-  arr := [5]int{22, 11, 33, 44, 55}
-  
-  //选择排序
-  for i := 0; i < len(arr)-1; i++ {
-    min := i
-    for j := i+1; j < len(arr); j++ {
-      if arr[j] < arr[min] {
-        min = j
-      }
-    }
-    if min != i {
-      arr[i], arr[min] = arr[min], arr[i]
-    }
-  }
-  
-  fmt.Pritnln("arr:", arr)
+	arr := [5]int{22, 11, 33, 44, 55}
+
+	//选择排序
+	for i := 0; i < len(arr)-1; i++ {
+		min := i
+		for j := i + 1; j < len(arr); j++ {
+			if arr[j] < arr[min] {
+				min = j
+			}
+		}
+		if min != i {
+			arr[i], arr[min] = arr[min], arr[i]
+		}
+	}
+
+	fmt.Println("arr:", arr)
 }
